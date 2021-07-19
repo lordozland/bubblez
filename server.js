348f204adef89683eb96fb5f90c82a8c93dd2640
William Crews <crewswh@gmail.com>
2021-07-19 15:34:44 -0400

    listening

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const http = require('http');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const path = require('path');
 const hbs = exphbs.create({});
 
 const PORT = 3333;
 
 const handleRequest = (request, response) => {
+    const path = req.url
     response.end(`It works! Path hit ${request.url}`)
-}
+};
 
-const server = http.createServer(handleRequest)
+const server = http.createServer(handleRequest);
 
-server.listen(PORT, () => console.log(`server is listening on local host ${PORT}`))
+server.listen(PORT, () => console.log(`server is listening on local host ${PORT}`));
