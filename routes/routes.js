2868c6e2d18a9f5420933b656f0d4d82b391fd2d
William Crews <crewswh@gmail.com>
2021-07-19 10:52:07 -0400

    struggles

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = require('express').Router();
+
 
 router.get('/', function(req, res, next) {
   res.render('dashboard', {title:Express});
 });
 
 router.get('/design', function(req, res, next) {
-    res.render('design', {title:Express});
-  	//res.send('respond with a resource');
+    res.render('design');
+});
+
+router.get('/life', function(req, res, next) {
+    res.render('life');
+});
+
+router.get('/navi', function(req, res, next) {
+    res.render('navi', {title:Express});
+});
+
+router.get('/reasons', function(req, res, next) {
+    res.render('reasons', {title:Express});
+});
+
+router.get('/sound', function(req, res, next) {
+    res.render('sound', {title:Express});
 });
 
 module.exports = router;
+
+// https://stackoverflow.com/questions/41527320/how-do-i-render-a-handlebars-page-from-a-routing-module
---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({});
+
+const PORT = 3333;
+
+const handleRequest = (request, response) => {
+    response.end('It works! Path hit ${request.url}')
+}
+
+const server = http.createServer(handleRequest)
+
+server.listen(PORT, () => console.log("server is listening on local host ${PORT}"))
+
