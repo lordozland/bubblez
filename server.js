d596fdc313f45e38b7041f800c9aeaa1766f2a7c
William Crews <crewswh@gmail.com>
2021-07-19 15:42:22 -0400

    message

--- server.js
+++ server.js
@@ -1,29 +1,42 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+// const express = require('express');
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
 
-const PORT = 3333;
+// const PORT = 3333;
 
-const handleRequest = (request, response) => {
-    const path = req.url
+// const handleRequest = (request, response) => {
+//     const path = req.url
 
-    response.end(`It works! Path hit ${request.url}`)
-};
+//     response.end(`It works! Path hit ${request.url}`)
+// };
+
+// const handleRequest = (req, res) => {
+//     const path = req.url;
+//     switch (path) {
+//       case '/':
+//         return displayRoot(res);
+  
+//       case '/portfolio':
+//         return displayPortfolio(res);
+  
+//       default:
+//         return display404(path, res);
+//     }
+//   };
+
+// const server = http.createServer(handleRequest);
+
+// server.listen(PORT, () => console.log(`server is listening on local host ${PORT}`));
+
+
+const http = require('http')
+const fs = require('fs')
+const PORT = 3333
 
 const handleRequest = (req, res) => {
-    const path = req.url;
-    switch (path) {
-      case '/':
-        return displayRoot(res);
-  
-      case '/portfolio':
-        return displayPortfolio(res);
-  
-      default:
-        return display404(path, res);
-    }
-  };
 
-const server = http.createServer(handleRequest);
+}
+
+const server http.createServer(handleRequest)
 
 server.listen(PORT, () => console.log(`server is listening on local host ${PORT}`));
